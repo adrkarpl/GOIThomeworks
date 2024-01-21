6a9136fa0fb82e89c37f545780cf66cefca715fe
adrkarpl <143037439+adrkarpl@users.noreply.github.com>
2024-01-22 00:29:04 +0100

    Update Procesy.py

--- Procesy_w_pythonie/Procesy.py
+++ Procesy_w_pythonie/Procesy.py
@@ -26,24 +26,24 @@ def factorize_parallel(*numbers):
 
 if __name__ == '__main__':
     #synchronous version
-    (s_time, [a, b, c, d]) = factorize_synchronous(128, 255, 99999, 10651060)
+    (synchronous_time, [a, b, c, d]) = factorize_synchronous(128, 255, 99999, 10651060)
     print(f"Synchronous version:\n{a}\n{b}\n{c}\n{d}\n")
     assert a == [1, 2, 4, 8, 16, 32, 64, 128]
     assert b == [1, 3, 5, 15, 17, 51, 85, 255]
     assert c == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
     assert d == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316, 380395, 532553, 760790, 1065106, 1521580, 2130212, 2662765, 5325530, 10651060]
-    print(f"Time: {s_time} s\n")
+    print(f"Time: {synchronous_time} s\n")
 
     #parallel version
-    (p_time, [e, f, g, h]) = factorize_parallel(128, 255, 99999, 10651060)
+    (parallel_time, [e, f, g, h]) = factorize_parallel(128, 255, 99999, 10651060)
     print(f"Parallel version:\n{e}\n{f}\n{g}\n{h}\n")
     assert e == [1, 2, 4, 8, 16, 32, 64, 128]
     assert f == [1, 3, 5, 15, 17, 51, 85, 255]
     assert g == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
     assert h == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316, 380395, 532553, 760790, 1065106, 1521580, 2130212, 2662765, 5325530, 10651060]
-    print(f"Time: {p_time} s\n")
+    print(f"Time: {parallel_time} s\n")
 
-    if p_time > s_time:
+    if parallel_time > synchronous_time:
         print("The synchronous version is faster.\n")
     else:
         print("The parallel version is faster.\n")
