18a96b5e4db2d70e66cd3243e90abf98e1cdde79
adrkarpl <143037439+adrkarpl@users.noreply.github.com>
2024-01-22 23:02:03 +0100

    Add files via upload

--- Clean_folder_program/clean_folder/clean.py
+++ Clean_folder_program/clean_folder/clean.py
@@ -8,7 +8,6 @@ from concurrent.futures import ThreadPoolExecutor
 import logging
 
 
-
 extensions = {
     'jpg': 'images',
     'jpeg': 'images',
@@ -35,20 +34,20 @@ extensions = {
 }
 
 
-# Konfiguracja loggera
+# logger configuration
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
 def transliterate_and_normalize(input_string):
-    # Tworzenie mapy transliteracji polskich znaków na znaki ASCII
+    # Creating a mapping for transliteration of Polish characters to ASCII characters
     transliteration_map = {
         'ą': 'a', 'ć': 'c', 'ę': 'e', 'ł': 'l',
         'ń': 'n', 'ó': 'o', 'ś': 's', 'ź': 'z',
         'ż': 'z',
     }
 
-    # Wykonaj transliterację na znaku po znaku i zamień inne znaki na '_'
+# Perform transliteration character by character and replace other characters with '_'
     normalized_string = ''
     for char in input_string:
         normalized_char = transliteration_map.get(char, char)
@@ -58,7 +57,7 @@ def transliterate_and_normalize(input_string):
 
     return normalized_string
 
-# Zapytanie czy na pewno ten katalog chce się uporządkować.
+# Ask whether the directory should be organized.
 
 
 def confirm_directory_cleanup(path):
@@ -71,7 +70,7 @@ def confirm_directory_cleanup(path):
         return False
 
 
-# Wyodrębnianie rozszerzeń, tworzenie folderów, przenoszenie plików
+# Extracting extensions, creating folders, moving files
 def organize_files_by_extension_worker(args):
     folder, extensions, verbose = args
     archives_folder = os.path.join(folder, 'archives')
@@ -92,35 +91,37 @@ def organize_files_by_extension_worker(args):
                     logger.info(f'Moving {src} to {dst}')
                 shutil.move(src, dst)
 
+
+# Running multiple threads for organizing files
 def organize_files_by_extension_parallel(path, extensions, verbose=False):
     with ThreadPoolExecutor() as executor:
         args_list = [(path, extensions, verbose) for _ in os.walk(path)]
         executor.map(organize_files_by_extension_worker, args_list)
 
 
-# Przenoszenie i rozpakowywanie spakowanych plików
+# Moving and unpacking compressed files
 def unpack_archives(archives_folder, path):
-    # Przeszukaj katalog "archives" w poszukiwaniu plików archive
+    # Search the "archives" directory for archive files
     for folder, subfolders, files in os.walk(archives_folder):
         for file in files:
             extension = file.split('.')[-1].lower()
             if extension in ('zip', 'gz', 'tar'):
                 archive_path = os.path.join(folder, file)
 
-                # Usuń rozszerzenie z nazwy archiwum
+                # Remove the file extension from the archive name
                 folder_name = os.path.splitext(file)[0]
                 if not os.path.exists(os.path.join(path, folder_name)):
                     os.mkdir(os.path.join(path, folder_name))
                 destination_path = os.path.join(archives_folder, folder_name)
 
-                # Jeśli katalog docelowy już istnieje, dodaj unikalny sufiks
+                # If the destination directory already exists, add a unique suffix
                 count = 1
                 while os.path.exists(destination_path):
                     destination_path = os.path.join(
                         archives_folder, f"{folder_name}_{count}")
                     count += 1
 
-                # Rozpakuj archiwum
+                # Unpack the archive
                 if extension == 'zip':
                     with zipfile.ZipFile(archive_path, 'r') as zip_ref:
                         zip_ref.extractall(destination_path)
@@ -131,35 +132,39 @@ def unpack_archives(archives_folder, path):
                     with tarfile.open(archive_path, 'r') as tar_ref:
                         tar_ref.extractall(destination_path)
 
-                # Usuń oryginalne archiwum
+                # Remove the original archive
                 if os.path.exists(archive_path):
                     os.remove(archive_path)
 
 
-# Funkcja zamiany liter polskich na zwykłe i normalizacja nazw plików i folderów
+# Function to convert Polish characters to standard ones and normalize file and folder names
 def normalize_and_rename_files(path):
     archives_folder = os.path.join(path, 'archives')
 
     for folder, subfolders, files in os.walk(path):
-        # Sprawdź, czy aktualny folder jest folderem "archives"
+
+        # Check if the current folder is the "archives" folder
+
         if os.path.normpath(folder) == os.path.normpath(archives_folder):
-            # Jeśli tak, pomiń ten folder i jego podfoldery
+
+            # If so, skip this folder and its subfolders
+
             subfolders[:] = []
             continue
         for file in files:
             old_file_path = Path(folder, file)
 
-            # Pobieranie rozszerzenie pliku
+            # Extracting the file extension
             file_extension = old_file_path.suffix
 
-            # usuwa rozszerzenie z nazwy pliku
+            # Removing the file extension from the file name
             file_name_without_extension = old_file_path.stem
 
-            # Wykonanie transliteracji polskich znaków na znaki ASCII i zamień inne znaki na '_'
+            # Perform transliteration of Polish characters to ASCII and replace other characters with '_'
             normalized_name = transliterate_and_normalize(
                 file_name_without_extension)
 
-            # Dodanie z powrotem rozszerzenia
+            # Adding the file extension back
             new_file_name = f"{normalized_name}{file_extension}"
             new_file_path = Path(folder, new_file_name)
 
@@ -169,25 +174,29 @@ def normalize_and_rename_files(path):
 def normalize_and_rename_folders(path):
     archives_folder = os.path.join(path, 'archives')
     for folder, subfolders, _ in os.walk(path, topdown=False):
-        # Sprawdź, czy aktualny folder jest folderem "archives"
+
+        # Check if the current folder is the "archives" folder
+
         if os.path.normpath(folder) == os.path.normpath(archives_folder):
-            # Jeśli tak, pomiń ten folder i jego podfoldery
+
+            # If so, skip this folder and its subfolders
+
             subfolders[:] = []
             continue
         for subfolder in subfolders:
             old_folder_path = Path(folder, subfolder)
 
-            # Transliteracja polskich znaków na znaki ASCII i zamień inne znaki na '_'
+            # Transliterate Polish characters to ASCII and replace other characters with '_'
             normalized_name = transliterate_and_normalize(subfolder)
 
-            # Dodanie z powrotem rozszerzenie
+            # Adding the file extension back
             new_folder_path = Path(folder, normalized_name)
 
-            # Przenoszenie (zmiana nazwy) folderu
+            # Moving (renaming) a folder
             old_folder_path.rename(new_folder_path)
 
 
-# Usuwa puste foldery
+# Remove empty folders
 def remove_empty_folders(path):
     for root, dirs, files in os.walk(path, topdown=False):
         for dir in dirs:
@@ -197,8 +206,8 @@ def remove_empty_folders(path):
                 print(f"[+] Removed empty folder: {folder_path}")
 
 
-# Podsumowanie
-# 1. Lista plików w każdej kategorii
+# Summary
+# 1. List of files in each category
 def list_files_in_categories(path, extensions):
     category_files = {category: [] for category in set(extensions.values())}
 
@@ -216,7 +225,7 @@ def list_files_in_categories(path, extensions):
             print(f"  {file}")
 
 
-# 2. Wykaz rozszerzeń, które pojawiły się w katalogach docelowych z podziałem na kategorie
+# 2. List of extensions that appeared in the target directories, categorized
 def list_extensions_in_categories(path, target_categories):
     target_categories = ['images', 'videos', 'documents', 'audio', 'archives']
     extensions_in_target_categories = {category: []
@@ -234,7 +243,7 @@ def list_extensions_in_categories(path, target_categories):
         print(f"{category}: {', '.join(ext_list)}")
 
 
-# 3. Wykaz nierozpoznanych rozszerzeń
+# 3. List of unrecognized extensions
 def list_unrecognized_extensions(path, extensions):
     unrecognized_extensions = set()
 
@@ -251,7 +260,7 @@ def list_unrecognized_extensions(path, extensions):
         print(f"  {ext}")
 
 
-# Wywołanie programu przy wpisaniu python sort.py lokalizacja pliku lub .\sort.py lokalizacja pliku przy PS
+# Running the program by typing python sort.py file_location or .\sort.py file_location in PowerShell
 def main(path):
     target_categories = ['images', 'videos', 'documents', 'audio', 'archives']
 
