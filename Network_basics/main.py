01b1ae2e115fd78dac797707342bf2fdb339448d
Adrian Karwat <adr.karwat@gmail.com>
2024-02-04 21:21:22 +0100

    mentor's comments

--- Network_basics/main.py
+++ Network_basics/main.py
@@ -4,15 +4,18 @@ import socket
 import urllib.parse
 from datetime import datetime
 from http.server import HTTPServer, BaseHTTPRequestHandler
+from http import HTTPStatus
 from pathlib import Path
 from threading import Thread
 
-# Define constants for socket configuration
+""" Define constants for socket configuration """
+
+
 SOCKET_IP = "127.0.0.1"
 SOCKET_PORT = 5005
 STORAGE_PATH = Path("storage")
 
-# Define HTTP request handler class
+""" Define HTTP request handler class """
 
 
 class HttpHandler(BaseHTTPRequestHandler):
@@ -29,15 +32,17 @@ class HttpHandler(BaseHTTPRequestHandler):
                 self.send_html_file(
                     Path('./front-init').joinpath('error.html'), 404)
 
-    # Handle POST requests
+    """ Handle POST requests """
+
     def do_POST(self):
         data = self.rfile.read(int(self.headers['Content-Length']))
         send_data_to_udp_socket(data)
-        self.send_response(302)
+        self.send_response(HTTPStatus.FOUND.value)
         self.send_header('Location', '/')
         self.end_headers()
 
-    # Send an HTML file as response
+    """ Send an HTML file as response """
+
     def send_html_file(self, filename, status=200):
         self.send_response(status)
         self.send_header('Content-type', 'text/html')
@@ -45,7 +50,8 @@ class HttpHandler(BaseHTTPRequestHandler):
         with open(filename, 'rb') as fd:
             self.wfile.write(fd.read())
 
-    # Send a static file as response
+    """ Send a static file as response """
+
     def send_static(self):
         file_path = f'./front-init/{self.path}'
         self.send_response(200)
@@ -58,7 +64,8 @@ class HttpHandler(BaseHTTPRequestHandler):
         with open(file_path, 'rb') as file:
             self.wfile.write(file.read())
 
-# Run the HTTP server
+
+""" Run the HTTP server """
 
 
 def run_http_server(server_class=HTTPServer, handler_class=HttpHandler):
@@ -69,7 +76,8 @@ def run_http_server(server_class=HTTPServer, handler_class=HttpHandler):
     except KeyboardInterrupt:
         http.server_close()
 
-# Save data to a JSON file
+
+""" Save data to a JSON file """
 
 
 def save_data(data):
@@ -85,7 +93,8 @@ def save_data(data):
     with open(data_path, "w", encoding="utf-8") as fh:
         json.dump(data_json, fh, indent=4, ensure_ascii=False)
 
-# Send data to the UDP socket server
+
+""" Send data to the UDP socket server """
 
 
 def send_data_to_udp_socket(data):
@@ -93,7 +102,8 @@ def send_data_to_udp_socket(data):
     udp_socket.sendto(data, (SOCKET_IP, SOCKET_PORT))
     udp_socket.close()
 
-# Run the UDP socket server
+
+""" Run the UDP socket server """
 
 
 def run_udp_socket_server(ip, port):
@@ -111,7 +121,7 @@ def run_udp_socket_server(ip, port):
         udp_socket.close()
 
 
-# Main function
+""" Main function """
 if __name__ == '__main__':
     print('The server has been successfully started!')
     if not STORAGE_PATH.exists():
